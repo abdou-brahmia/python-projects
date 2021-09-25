4e0b9a7e17b658994f3af64a75d428b78cea250b
brahmia abd el bacet <44529061+abdou-brahmia@users.noreply.github.com>
2021-09-25 02:51:07 +0100

    Add files via upload

--- dichotomous search/dichotomous search.py
+++ dichotomous search/dichotomous search.py
@@ -1,1 +1,22 @@
+def recherche(liste_temp,element):
+    
+    liste=sorted(liste_temp)
+    print(liste)
+    a = 0
+    b = len(liste)-1
+    state=False
+    while (a <= b) and (state==False):
+        m = (a+b)//2
+        if liste[m] == element:
+            state =True
+        elif liste[m] > element:
+            b = m-1
+        elif liste[m] < element:
+            a = m+1
+    return [state,m]
 
+if __name__ == "__main__":
+    
+    li = [5, 3, 2, 4, 6]
+    a = recherche(li,3)
+    print(a)
